39530526da29931bf786bd10730b9dd82597bbbe
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-04-11 16:38:26 +0530

    feat: add async handler and implement in user controller

--- apps/server/src/modules/user/user.controller.js
+++ apps/server/src/modules/user/user.controller.js
@@ -1,287 +1,251 @@
 import config from "../../common/config/config.js"
 import { userModel } from "./user.modal.js"
 import { sessionModel } from "../session/session.modal.js"
+import { asyncHandler } from "../../common/utils/async-handler.js"
 import crypto from "crypto"
 import jwt from "jsonwebtoken"
 
-const registerUser = async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
 
-    // check if user already exists
-    const existingUser = await userModel.findOne({ email })
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" })
-    }
-
-    // hash the password using crypto
-    const passwordHash = crypto
-      .createHash("sha256")
-      .update(password)
-      .digest("hex")
-
-    // create a new user in the database
-    const newUser = await userModel.create({
-      first_name: firstName,
-      last_name: lastName,
-      email,
-      password_hash: passwordHash,
-      instance_id: config.systemInstanceId,
-    })
-
-    res.status(201).json({
-      message: "User registered successfully",
-      user: {
-        firstName: newUser.first_name,
-        lastName: newUser.last_name,
-        email: newUser.email,
-      },
-    })
-  } catch (error) {
-    res.status(500).json({ message: "Error registering user" })
-    console.log("error", error.message)
+  // check if user already exists
+  const existingUser = await userModel.findOne({ email })
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exists" })
   }
-}
 
-const loginUser = async (req, res) => {
-  try {
-    // parse data from request body
-    const { email, password } = req.body
+  // hash the password using crypto
+  const passwordHash = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex")
 
-    //  check if user exists
-    const existingUser = await userModel.findOne({ email })
-    if (!existingUser) {
-      return res.status(400).json({ message: "Invalid email or password" })
-    }
+  // create a new user in the database
+  const newUser = await userModel.create({
+    first_name: firstName,
+    last_name: lastName,
+    email,
+    password_hash: passwordHash,
+    instance_id: config.systemInstanceId,
+  })
 
-    // match the password
-    const passwordHash = crypto
-      .createHash("sha256")
-      .update(password)
-      .digest("hex")
+  res.status(201).json({
+    message: "User registered successfully",
+    user: {
+      firstName: newUser.first_name,
+      lastName: newUser.last_name,
+      email: newUser.email,
+    },
+  })
+})
 
-    if (passwordHash !== existingUser.password_hash) {
-      return res.status(400).json({ message: "Invalid email or password" })
-    }
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
 
-    // generate a JWT refresh token
-    const refreshToken = jwt.sign(
-      {
-        userId: existingUser._id,
-      },
-      config.jwtSecret,
-      { expiresIn: config.jwtRefreshTokenExpiration }
-    )
-
-    const refreshTokenHash = crypto
-      .createHash("sha256")
-      .update(refreshToken)
-      .digest("hex")
-
-    // create session in db
-    await sessionModel.create({
-      user_id: existingUser._id,
-      refresh_token_hash: refreshTokenHash,
-    })
-
-    // generate a JWT access token
-    const accessToken = jwt.sign(
-      {
-        userId: existingUser._id,
-      },
-      config.jwtSecret,
-      { expiresIn: config.jwtAccessTokenExpiration }
-    )
-
-    // set refresh token in cookie
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    })
-
-    // return success response with user data or error message
-    res.status(200).json({
-      message: "User logged in successfully",
-      user: {
-        email: existingUser.email,
-      },
-      token: accessToken,
-    })
-  } catch (error) {
-    res.status(500).json({ message: "Error logging in user" })
-    console.log("error", error.message)
+  //  check if user exists
+  const existingUser = await userModel.findOne({ email })
+  if (!existingUser) {
+    return res.status(400).json({ message: "Invalid email or password" })
   }
-}
 
-const getMe = async (req, res) => {
-  try {
-    // parse access token from request header
-    const accessToken = req.headers.authorization?.split(" ")[1]
-    if (!accessToken) {
-      return res.status(401).json({ message: "Access token missing" })
-    }
+  // match the password
+  const passwordHash = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex")
 
-    // decode the access token
-    const decoded = jwt.verify(accessToken, config.jwtSecret)
-
-    // find the user by id
-    const user = await userModel.findById(decoded.userId)
-    if (!user) {
-      return res.status(404).json({ message: "User not found" })
-    }
-
-    res.status(200).json({
-      message: "Current user fetched successfully",
-      user: {
-        firstName: user.first_name,
-        lastName: user.last_name,
-        email: user.email,
-      },
-    })
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching current user" })
-    console.log("error", error.message)
+  if (passwordHash !== existingUser.password_hash) {
+    return res.status(400).json({ message: "Invalid email or password" })
   }
-}
 
-const refreshToken = async (req, res) => {
-  try {
-    // parse refresh token from cookies
-    const refreshToken = req.cookies.refreshToken
-    if (!refreshToken) {
-      return res.status(401).json({ message: "Refresh token missing" })
-    }
+  // generate a JWT refresh token
+  const refreshToken = jwt.sign(
+    {
+      userId: existingUser._id,
+    },
+    config.jwtSecret,
+    { expiresIn: config.jwtRefreshTokenExpiration }
+  )
 
-    const refreshTokenHash = crypto
-      .createHash("sha256")
-      .update(refreshToken)
-      .digest("hex")
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex")
 
-    // find the session with valid refresh token hash and not revoked
-    const session = await sessionModel.findOne({
-      refresh_token_hash: refreshTokenHash,
-      is_revoked: false,
-    })
+  // create session in db
+  await sessionModel.create({
+    user_id: existingUser._id,
+    refresh_token_hash: refreshTokenHash,
+  })
 
-    if (!session) {
-      return res.status(401).json({ message: "Invalid refresh token" })
-    }
+  // generate a JWT access token
+  const accessToken = jwt.sign(
+    {
+      userId: existingUser._id,
+    },
+    config.jwtSecret,
+    { expiresIn: config.jwtAccessTokenExpiration }
+  )
 
-    // generate new access token and refresh token
-    const decoded = jwt.verify(refreshToken, config.jwtSecret)
+  // set refresh token in cookie
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  })
 
-    const newAccessToken = jwt.sign(
-      {
-        userId: decoded._id,
-      },
-      config.jwtSecret,
-      { expiresIn: config.jwtAccessTokenExpiration }
-    )
+  // return success response with user data or error message
+  res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+      email: existingUser.email,
+    },
+    token: accessToken,
+  })
+})
 
-    const newRefreshToken = jwt.sign(
-      {
-        userId: decoded._id,
-      },
-      config.jwtSecret,
-      { expiresIn: config.jwtRefreshTokenExpiration }
-    )
-
-    const newRefreshTokenHash = crypto
-      .createHash("sha256")
-      .update(newRefreshToken)
-      .digest("hex")
-
-    // update session with new refresh token hash
-    session.refresh_token_hash = newRefreshTokenHash
-    await session.save()
-
-    res.cookie("refreshToken", newRefreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    })
-
-    res.status(200).json({
-      message: "Token refreshed successfully",
-      token: newAccessToken,
-    })
-  } catch (error) {
-    res.status(500).json({ message: "Error refreshing token" })
-    console.log("error", error.message)
+const getMe = asyncHandler(async (req, res) => {
+  const accessToken = req.headers.authorization?.split(" ")[1]
+  if (!accessToken) {
+    return res.status(401).json({ message: "Access token missing" })
   }
-}
 
-const logoutUser = async (req, res) => {
-  try {
-    // parse refresh token from cookies
-    const refreshToken = req.cookies.refreshToken
-    if (!refreshToken) {
-      return res.status(401).json({ message: "Refresh token missing" })
-    }
+  // decode the access token
+  const decoded = jwt.verify(accessToken, config.jwtSecret)
 
-    // hash the refresh token
-    const refreshTokenHash = crypto
-      .createHash("sha256")
-      .update(refreshToken)
-      .digest("hex")
-
-    // find the session with valid refresh token hash and not revoked
-    const session = await sessionModel.findOne({
-      refresh_token_hash: refreshTokenHash,
-      is_revoked: false,
-    })
-
-    if (!session) {
-      return res.status(401).json({ message: "Invalid refresh token" })
-    }
-
-    // revoke the session
-    session.is_revoked = true
-    await session.save()
-
-    // clear the refresh token cookie
-    res.clearCookie("refreshToken")
-
-    res.status(200).json({ message: "User logged out successfully" })
-  } catch (error) {
-    res.status(500).json({ message: "Error logging out user" })
-    console.log("error", error.message)
+  // find the user by id
+  const user = await userModel.findById(decoded.userId)
+  if (!user) {
+    return res.status(404).json({ message: "User not found" })
   }
-}
 
-const logoutUserAllSessions = async (req, res) => {
-  try {
-    // parse refresh token from cookies
-    const refreshToken = req.cookies.refreshToken
-    if (!refreshToken) {
-      return res.status(401).json({ message: "Refresh token missing" })
-    }
+  res.status(200).json({
+    message: "Current user fetched successfully",
+    user: {
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+    },
+  })
+})
 
-    // decode the refresh token
-    const decoded = jwt.verify(refreshToken, config.jwtSecret)
-
-    // revoke all sessions for the user
-    await sessionModel.updateMany(
-      { user_id: decoded.userId },
-      { $set: { is_revoked: true } }
-    )
-
-    // clear the refresh token cookie
-    res.clearCookie("refreshToken")
-
-    res
-      .status(200)
-      .json({ message: "User logged out from all sessions successfully" })
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error logging out user from all sessions" })
-    console.log("error", error.message)
+const refreshToken = asyncHandler(async (req, res) => {
+  const refreshToken = req.cookies.refreshToken
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Refresh token missing" })
   }
-}
+
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex")
+
+  // find the session with valid refresh token hash and not revoked
+  const session = await sessionModel.findOne({
+    refresh_token_hash: refreshTokenHash,
+    is_revoked: false,
+  })
+
+  if (!session) {
+    return res.status(401).json({ message: "Invalid refresh token" })
+  }
+
+  // generate new access token and refresh token
+  const decoded = jwt.verify(refreshToken, config.jwtSecret)
+
+  const newAccessToken = jwt.sign(
+    {
+      userId: decoded._id,
+    },
+    config.jwtSecret,
+    { expiresIn: config.jwtAccessTokenExpiration }
+  )
+
+  const newRefreshToken = jwt.sign(
+    {
+      userId: decoded._id,
+    },
+    config.jwtSecret,
+    { expiresIn: config.jwtRefreshTokenExpiration }
+  )
+
+  const newRefreshTokenHash = crypto
+    .createHash("sha256")
+    .update(newRefreshToken)
+    .digest("hex")
+
+  // update session with new refresh token hash
+  session.refresh_token_hash = newRefreshTokenHash
+  await session.save()
+
+  res.cookie("refreshToken", newRefreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  })
+
+  res.status(200).json({
+    message: "Token refreshed successfully",
+    token: newAccessToken,
+  })
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+  const refreshToken = req.cookies.refreshToken
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Refresh token missing" })
+  }
+
+  // hash the refresh token
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex")
+
+  // find the session with valid refresh token hash and not revoked
+  const session = await sessionModel.findOne({
+    refresh_token_hash: refreshTokenHash,
+    is_revoked: false,
+  })
+
+  if (!session) {
+    return res.status(401).json({ message: "Invalid refresh token" })
+  }
+
+  // revoke the session
+  session.is_revoked = true
+  await session.save()
+
+  // clear the refresh token cookie
+  res.clearCookie("refreshToken")
+
+  res.status(200).json({ message: "User logged out successfully" })
+})
+
+const logoutUserAllSessions = asyncHandler(async (req, res) => {
+  const refreshToken = req.cookies.refreshToken
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Refresh token missing" })
+  }
+
+  // decode the refresh token
+  const decoded = jwt.verify(refreshToken, config.jwtSecret)
+
+  // revoke all sessions for the user
+  await sessionModel.updateMany(
+    { user_id: decoded.userId },
+    { $set: { is_revoked: true } }
+  )
+
+  // clear the refresh token cookie
+  res.clearCookie("refreshToken")
+
+  res
+    .status(200)
+    .json({ message: "User logged out from all sessions successfully" })
+})
 
 export {
   registerUser,
